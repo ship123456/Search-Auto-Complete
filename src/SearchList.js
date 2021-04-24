d8951950c7023aecfc2404854a16d0fafd49e3fb
Shipra Tuteja <54984794+ship123456@users.noreply.github.com>
2021-04-25 00:24:17 +0530

    Add files via upload

---
+++ src/SearchList.js
@@ -0,0 +1,5 @@
+const SearchList=
+['apple', 'mango', 'banana','orange','kiwi','watermelon','melon','sugrcane','blueberries','strawberry','cherries','leeches','coconut']
+
+
+export default SearchList
